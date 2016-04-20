4f2cd3846f5d222c815d7409b5c313d8bb555c2d
Robert Winterbottom <rwinterbottom@blueraster.com>
2016-04-20 09:52:40 -0400

    Removed layer count from TOC

--- src/js/components/LayerPanel/LayerGroup.js
+++ src/js/components/LayerPanel/LayerGroup.js
@@ -9,16 +9,17 @@ let closeSymbolCode = 9660,
 * @param {array} activeLayers - array of keys for the active layers
 * @param {array} children - This groups child components, which are layer checkboxes or null
 * @return {number} count
+* <span className='active-layer-count'>({getCount(this.props.activeLayers, this.props.children)})</span>
 */
-let getCount = (activeLayers, children) => {
-  let count = 0;
-  children.forEach(layer => {
-    if (layer && layer.key && activeLayers.indexOf(layer.key) > -1) {
-      ++count;
-    }
-  });
-  return count;
-};
+// let getCount = (activeLayers, children) => {
+//   let count = 0;
+//   children.forEach(layer => {
+//     if (layer && layer.key && activeLayers.indexOf(layer.key) > -1) {
+//       ++count;
+//     }
+//   });
+//   return count;
+// };
 
 export default class LayerGroup extends React.Component {
 
@@ -30,7 +31,6 @@ export default class LayerGroup extends React.Component {
       <div className='layer-category'>
         <div className='layer-category-label pointer' onClick={this.toggle}>
           {this.props.label}
-          <span className='active-layer-count'>({getCount(this.props.activeLayers, this.props.children)})</span>
           <span className='layer-category-caret'>{String.fromCharCode(active ? closeSymbolCode : openSymbolCode)}</span>
         </div>
         <div className={`layer-category-content ${active ? '' : 'closed'}`}>{this.props.children}</div>
