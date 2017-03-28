c16b4b11f2009c15f1d5c6b1f30fdc8fc07cff10
Lucas Cotner <lc07@uw.edu>
2017-03-28 17:17:56 -0400

    Updated our tree cover loss layer type, integrated our custom layer

--- src/js/utils/layerFactory.js
+++ src/js/utils/layerFactory.js
@@ -7,6 +7,7 @@ import GraphicsLayer from 'esri/layers/GraphicsLayer';
 import FeatureLayer from 'esri/layers/FeatureLayer';
 import TerraILayer from 'js/layers/TerraILayer';
 import GladLayer from 'js/layers/GladLayer';
+import TreeCoverLossLayer from 'js/layers/TreeCoverLossLayer';
 import layerUtils from 'utils/layerUtils';
 import {errors} from 'js/config';
 
@@ -90,6 +91,15 @@ export default (layer, lang) => {
       options.confidence = layer.confidence;
       options.visible = layer.visible || false;
       esriLayer = new GladLayer(options);
+    break;
+    case 'loss':
+      options.id = layer.id;
+      options.url = layer.url;
+      options.minYear = layer.minYear;
+      options.maxYear = layer.maxYear;
+      // options.confidence = layer.confidence;
+      options.visible = layer.visible || false;
+      esriLayer = new TreeCoverLossLayer(options);
     break;
     case 'terra':
       layer.visible = layer.visible || false;
