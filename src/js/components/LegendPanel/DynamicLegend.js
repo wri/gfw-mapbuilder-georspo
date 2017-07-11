8109aef8bd8c4142d1cb6da5cd41b580ecd9ae6c
Timur Girgin <tgirgin@blueraster.com>
2017-07-11 16:02:08 -0400

    removed logs

--- src/js/components/LegendPanel/DynamicLegend.js
+++ src/js/components/LegendPanel/DynamicLegend.js
@@ -4,13 +4,11 @@ import React from 'react';
 export default class DynamicLegend extends React.Component {
  
     constructor (props) {
-       console.log('DynamicLegend + CONSTRUCTOR');
     super(props);
     this.state = { legendInfos: [] };
   }
 
   componentDidMount() {
-     console.log('DynamicLegend + CONSTRUCTOR');
     Request.getLegendInfos(this.props.url, this.props.layerIds).then(legendInfos => {
       if(this.refs.myRef) {
         this.setState({ legendInfos: legendInfos });
@@ -23,7 +21,6 @@ export default class DynamicLegend extends React.Component {
   }
 
   itemMapper (item, index) {
-     console.log('DynamicLegend + CONSTRUCTOR');
     return (
       <div className='legend-row' key={index}>
         <img className='legend-icon' title={item.label} src={`data:image/png;base64,${item.imageData}`} />
@@ -33,7 +30,6 @@ export default class DynamicLegend extends React.Component {
   }
 
   render () {
-     console.log('DynamicLegend + CONSTRUCTOR');
     return (
       <div className='legend-container' ref="myRef">
         {this.state.legendInfos.length === 0 ? <div className='legend-unavailable'>No Legend</div> :
