d01134dc6eea8cd7146f80a59a2cc65730ad1e77
Lucas Cotner <lc07@uw.edu>
2017-07-20 14:24:17 -0400

    sticking with our new exernal template and removing console logs

--- src/js/library.js
+++ src/js/library.js
@@ -8,13 +8,10 @@ var MapBuilder = function(args){
         newBase = scripts[j].src;
       }
     }
-    console.log('newBase', newBase);
 
     if (newBase) {
       resourcesBase = newBase.split(constructorParams.version)[0];
       newBase = resourcesBase + constructorParams.version + '/';
-      console.log('newweBase', newBase);
-      console.log('resourcesBase', resourcesBase);
     }
 
     window._app = {
@@ -48,9 +45,6 @@ var MapBuilder = function(args){
       constructorParams.basePath = base;
     }
     base = makePath(base);
-    console.log('base', base);
-    console.log('resourcesBase', resourcesBase);
-    console.log('root', getResourcePath(resourcesBase.replace(/\/[^/]+$/, '')));
 
     window.dojoConfig = {
       parseOnLoad: false,
